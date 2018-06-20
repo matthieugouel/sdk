a0a0e7eac45fd91ad32c4f5a3296c2beed78e913
Bert Casula <thebertster+github@gmail.com>
2018-06-20 17:21:37 +0100

    Refactor and add help text.

--- python/avi/sdk/samples/clone_vs.py
+++ python/avi/sdk/samples/clone_vs.py
@@ -19,7 +19,7 @@ urllib3.disable_warnings()
 
 DEFAULT_API_VERSION = '16.4.2'
 
-AVICLONE_VERSION = [1, 0, 4]
+AVICLONE_VERSION = [1, 0, 5]
 
 # Try to obtain the terminal width to allow spprint() to wrap output neatly.
 # If unable to determine, assume terminal width is 70 characters
@@ -31,8 +31,9 @@ except:
 
 def spprint(s, ind='', **kwargs):
     flush = kwargs.pop('flush', False)
-    print('\r\n'.join(textwrap.wrap(s, width=T_SIZE, subsequent_indent=ind,
-                                    break_on_hyphens=False)), **kwargs)
+    for para in s.splitlines():
+        print('\r\n'.join(textwrap.wrap(para, width=T_SIZE, subsequent_indent=ind,
+                                        break_on_hyphens=False)), **kwargs)
     if flush:
         f = kwargs.get('file', sys.stdout)
         f.flush() if f is not None else sys.stdout.flush()
@@ -109,6 +110,30 @@ class AviClone:
 
         return obj
 
+    def _get_obj_info(self, obj_type, obj=None, obj_name=None, api_to_use=None):
+        """
+        Return object name, UUID and object representation from an object
+        passed either as an object or object name
+        """
+
+        api_to_use = api_to_use or self.api
+
+        if obj:
+            obj_name = obj.get('name', None)
+            obj_uuid = obj.get('uuid', None)
+        else:
+            if obj_name is None:
+                obj = None
+                obj_uuid = None
+            else:
+                obj = api_to_use.get_object_by_name(obj_type, obj_name)
+                if obj is None:
+                    raise Exception('A %s with name %s could not be found'
+                                    % obj_type)
+                obj_uuid = obj.get('uuid', None)
+
+        return obj, obj_name, obj_uuid
+
     def _delete_created_objs(self, created_objs, otenant_uuid):
         """
         Deletes any created objects when a failure has occurred.
@@ -138,15 +163,9 @@ class AviClone:
         :param tenant: Tenant containing the objects
         """
 
-        if tenant is None:
-            t_obj = None
-            tenant_uuid = None
-        else:
-            t_obj = self.dest_api.get_object_by_name('tenant', tenant)
-            if t_obj is None:
-                raise Exception('A tenant with name %s could not be found'
-                                % tenant)
-            tenant_uuid = t_obj['uuid']
+        t_obj, tenant, tenant_uuid = self._get_obj_info(obj_type='tenant',
+                                                        obj_name=tenant,
+                                                        api_to_use=self.dest_api)
 
         self._delete_created_objs(objs, tenant_uuid)
 
@@ -192,45 +211,26 @@ class AviClone:
         # Retrieve tenant, other_tenant and other_cloud names, uuids and objects
         # which may have been passed as objects or names
 
-        if t_obj:
-            tenant = t_obj['name']
-            tenant_uuid = t_obj['uuid']
-        else:
-            if tenant is None:
-                t_obj = None
-                tenant_uuid = None
-            else:
-                t_obj = self.api.get_object_by_name('tenant', tenant)
-                if t_obj is None:
-                    raise Exception('A tenant with name %s could not be found'
-                                    % tenant)
-                tenant_uuid = t_obj['uuid']
+        t_obj, tenant, tenant_uuid = self._get_obj_info(obj_type='tenant',
+                                                        obj=t_obj,
+                                                        obj_name=tenant)
 
-        if ot_obj:
-            other_tenant = ot_obj['name']
-            otenant_uuid = ot_obj['uuid']
-        else:
-            if other_tenant is None:
-                ot_obj = None
-                otenant_uuid = tenant_uuid
-            else:
-                ot_obj = self.dest_api.get_object_by_name('tenant',
-                                                          other_tenant)
-                if ot_obj is None:
-                    raise Exception('A tenant with name %s could not be found'
-                                    % other_tenant)
-                otenant_uuid = ot_obj['uuid']
+        if ot_obj is None and other_tenant is None:
+            # Assume cloning to a tenant of the same name as the source
+            # tenant if not otherwise specified
 
-        if oc_obj:
-            other_cloud = oc_obj['name']
-        else:
-            if other_cloud is None:
-                oc_obj = None
-            else:
-                oc_obj = self.dest_api.get_object_by_name('cloud', other_cloud)
-                if oc_obj is None:
-                    raise Exception('A cloud with name %s could not be found'
-                                    % other_cloud)
+            other_tenant = tenant
+
+        ot_obj, other_tenant, otenant_uuid = self._get_obj_info(
+                                                         obj_type='tenant',
+                                                         obj=ot_obj,
+                                                         obj_name=other_tenant,
+                                                         api_to_use=self.dest_api)
+        
+        oc_obj, other_cloud, ocloud_uuid = self._get_obj_info(obj_type='cloud',
+                                                 obj=oc_obj,
+                                                 obj_name=other_cloud,
+                                                 api_to_use=self.dest_api)
 
         if not object_type:
             # If object_type is not specified, assume the old_name is in
@@ -1104,33 +1104,19 @@ class AviClone:
         new_vs_fips = new_vs_fips or [None * len(new_vs_vips)]
         new_fqdns = new_fqdns or ['*']
 
-        if tenant is None:
-            t_obj = None
-            tenant_uuid = None
-        else:
-            t_obj = self.api.get_object_by_name('tenant', tenant)
-            if t_obj is None:
-                raise Exception('A tenant with name %s could not be found'
-                                % tenant)
-            tenant_uuid = t_obj['uuid']
+        t_obj, tenant, tenant_uuid = self._get_obj_info(obj_type='tenant',
+                                                        obj_name=tenant)
 
-        if other_tenant is None:
-            ot_obj = None
-            otenant_uuid = tenant_uuid
-        else:
-            ot_obj = self.dest_api.get_object_by_name('tenant', other_tenant)
-            if ot_obj is None:
-                raise Exception('A tenant with name %s could not be found'
-                                % other_tenant)
-            otenant_uuid = ot_obj['uuid']
+        other_tenant = other_tenant or tenant
 
-        if other_cloud is None:
-            oc_obj = None
-        else:
-            oc_obj = self.dest_api.get_object_by_name('cloud', other_cloud)
-            if oc_obj is None:
-                raise Exception('A cloud with name %s could not be found'
-                                % other_cloud)
+        ot_obj, other_tenant, otenant_uuid = self._get_obj_info(
+                                                         obj_type='tenant',
+                                                         obj_name=other_tenant,
+                                                         api_to_use=self.dest_api)
+        
+        oc_obj, other_cloud, ocloud_uuid = self._get_obj_info(obj_type='cloud',
+                                                 obj_name=other_cloud,
+                                                 api_to_use=self.dest_api)
 
         if new_vs_fips != [None] and len(new_vs_vips) != len(new_vs_fips):
             raise Exception('Cannot clone virtual service if number of VIPs '
@@ -1910,106 +1896,95 @@ if __name__ == '__main__':
             print()
             print(ex)
     else:
-        parser.print_help()
+        # If run with no parameters, display general help text for the script
 
-'''
-Notes:
-
-For help, use:
-
-clone_vs.py -h
-
-For detailed help on cloning a specific object, use for example:
-
-clone_vs.py vs -h
-
-The script allows the cloning of virtual services, pools, pool groups and
-generic objects (that have no child references).
-
-The script clones any additional objects that are required, so for example
-when cloning a virtual service, the pools and/or pool groups used by that
-VS, including any specified in context-switching rules, will also be cloned.
-
-The script also allows the cloning of objects into a different tenant and/or 
-cloud than the source - the destination may also be on a different Avi Vantage
-controller.
-
-These advanced cloning options may not always be successful because the source 
-and destination tenant/cloud/controllers may have different properties. If the
-clone attempt fails, the script will automatically delete any objects that it
-created along the way.
-
-By default, re-usable objects/profiles such as application profiles, health
-monitors, persistency profiles etc. are not cloned so that the cloned object
-simply refers to the same simply refers to the same objects as the original.
-However, when cloning to a different tenant, the behaviour is slightly
-different:
-
-If a re-usable object was defined in the admin tenant, it is available in all
-tenants and will not be cloned by default. If a re-usable objects was defined
-in the source tenant, the script looks for an object with the same name in the
-destination tenant first, but if no identically-named object is available, the
-object will be cloned to the destination.
-
-This behaviour can be overridden using the "forceclone" option, which ensures
-that the specified references are always cloned rather than re-used.
-
-For example, to forcibly clone all health monitors, use the option:
-
---forceclone pool-healthmonitor
-
-The full list of supported options is displayed in the help.
-
-Examples:
-
-* Cloning a VS and child objects within a tenant:
-
-clone_vs.py -c controller.acme.com -x 17.2.9 vs example cloned-example
-
-* Cloning a VS and child objects to a different tenant:
-
-clone_vs.py -c controller.acme.com -x 17.2.9 vs example cloned-example
-  -t tenant1 -2t tenant2
-
-* Cloning a VS but forcing health monitors and application profiles to
-be cloned rather than re-used in the cloned VS:
-
-clone_vs.py -c controller.acme.com -x 17.2.9 vs example cloned-example
-  -fc pool-healthmonitor,vs-appprofile
-
-* Cloning an application profile to a different tenant on a different
-  controller:
-
-clone_vs.py -c controller1.acme.com -dc controller2.acme.com -x 17.2.9
-  generic health-monitor cloned-health-monitor -t tenant1 -2t tenant2
-  -2c Default-Cloud
-
-* Cloning a VS to a different controller with an AWS cloud, specifying
-  auto-allocation for VIPs by subnet in 3 AZs:
-
-clone_vs.py -c controller1.acme.com -dc controller2.acme.com -x 17.2.9
-  example cloned-example -v 10.0.0.0/24,10.1.0.0/24,10.2.0.0/24
-  -t tenant -2t tenant -2c AWS-Cloud
-
-* As above but also with elastic IP allocation:
-
-clone_vs.py -c controller1.acme.com -dc controller2.acme.com -x 17.2.9
-  example cloned-example -v 10.0.0.0/24,10.1.0.0/24,10.2.0.0/24;*,*,*
-  -t tenant -2t tenant -2c AWS-Cloud
-
-If the object to be cloned uses features specific to a particular minimum Avi
-Vantage s/w release, it may be necessary to specify the API version using the
--x parameter.
-
-Some known limitations:
-* Cloning a VS to a cloud of a different type to the source cloud is more
-  likely to fail as it may reference shared objects which do not make sense in
-  the destination cloud
-* Cloning a VS to a different tenant/cloud will try to find an SE group with
-  the same name as referenced in the source VS but if no match is found, will
-  place into the Default SE group instead
-* The script has been primarily written for and tested with Linux/VMWare/AWS
-  clouds - it may not work as-is for other clouds
-* Under bash on Linux, remember to escape the * character with a backslash if
-  passed as a parameter (e.g. in -v).
-'''
+        spprint(
+            'For syntax help, use:\r\n'
+            'clone_vs.py -h\r\n\r\n'
+            'For detailed help on cloning a specific object, use for example:\r\n'
+            'clone_vs.py vs -h\r\n\r\n'
+            'The script allows the cloning of virtual services, pools, pool '
+            'groups and generic objects (that have no child references).\r\n\r\n'
+            'The script clones any additional objects that are required, so for '
+            'example when cloning a virtual service, the pools and/or pool '
+            'groups used by that VS, including any specified in '
+            'context-switching rules, will also be cloned.\r\n\r\n'
+            'The script also allows the cloning of objects into a different '
+            'tenant and/or cloud than the source - the destination may also be '
+            'on a different Avi Vantage controller.\r\n\r\n'
+            'These advanced cloning options may not always be successful because '
+            'the source and destination tenant/cloud/controllers may have '
+            'different properties. If the clone attempt fails, the script will '
+            'automatically delete any objects that it created along the '
+            'way.\r\n\r\n'
+            'By default, re-usable child objects/profiles such as application '
+            'profiles, health monitors, persistency profiles etc. are not '
+            'cloned so that the cloned object simply refers to the same '
+            'child objects as the original.\r\n\r\n'
+            'However, when cloning to a different tenant, the behaviour is '
+            'slightly different:\r\n\r\n'
+            'If a re-usable object was defined in the admin tenant, it is '
+            'available in all tenants and will not be cloned by default. If a '
+            're-usable objects was defined in the source tenant, the script '
+            'will use an identically-named object in the target tenant if one '
+            'is found. Otherwise, the object will be cloned to the '
+            'destination.\r\n\r\n'
+            'This behaviour can be overridden using the "forceclone" option '
+            'which ensures that the specified references are always cloned '
+            'rather than re-used.\r\n\r\n'
+            'For example, to forcibly clone all health monitors, use the '
+            'option:\r\n\r\n'
+            '--forceclone pool-healthmonitor\r\n\r\n'
+            'The full list of supported options is displayed in the help.\r\n\r\n'
+            'Examples:\r\n\r\n'
+            '* Cloning a VS and child objects within a tenant:\r\n\r\n')
+        print(
+            'clone_vs.py -c controller.acme.com -x 17.2.9 vs example '
+            'cloned-example\r\n\r\n')
+        spprint(
+            '* Cloning a VS and child objects to a different tenant:\r\n\r\n')
+        print(
+            'clone_vs.py -c controller.acme.com -x 17.2.9 vs example '
+            'cloned-example -t tenant1 -2t tenant2\r\n\r\n')
+        spprint(
+            '* Cloning a VS but forcing health monitors and application '
+            'profiles to be cloned rather than re-used in the cloned VS:\r\n\r\n')
+        print(
+            'clone_vs.py -c controller.acme.com -x 17.2.9 vs example '
+            'cloned-example -fc pool-healthmonitor,vs-appprofile\r\n\r\n')
+        spprint(
+            '* Cloning an application profile to a different tenant on a '
+            'different controller:\r\n\r\n')
+        print(
+            'clone_vs.py -c controller1.acme.com -dc controller2.acme.com -x '
+            '17.2.9 generic health-monitor cloned-health-monitor -t tenant1 '
+            '-2t tenant2 -2c Default-Cloud\r\n\r\n')
+        spprint(
+            '* Cloning a VS to a different controller with an AWS cloud, '
+            'specifying auto-allocation for VIPs by subnet in 3 AZs:\r\n\r\n')
+        print(
+            'clone_vs.py -c controller1.acme.com -dc controller2.acme.com -x '
+            '17.2.9 example cloned-example -v 10.0.0.0/24,10.1.0.0/24,'
+            '10.2.0.0/24 -t tenant -2t tenant -2c AWS-Cloud\r\n\r\n')
+        spprint('* As above but also with elastic IP allocation:\r\n\r\n')
+        print(
+            'clone_vs.py -c controller1.acme.com -dc controller2.acme.com -x '
+            '17.2.9 example cloned-example -v 10.0.0.0/24,10.1.0.0/24,'
+            '10.2.0.0/24;*,*,* -t tenant -2t tenant -2c AWS-Cloud\r\n\r\n')
+        spprint(
+            'If the object to be cloned uses features specific to a particular '
+            'minimum Avi Vantage s/w release, it may be necessary to specify '
+            'the API version using the -x parameter.\r\n\r\n'
+            'Some known limitations:\r\n\r\n'
+            '* Cloning a VS to a cloud of a different type to the source cloud '
+            'is more likely to fail as it may reference shared objects which '
+            'do not make sense in the destination cloud.\r\n\r\n'
+            '* Cloning a VS to a different tenant/cloud will try to find an SE '
+            'group with the same name as referenced in the source VS but if no '
+            'match is found, will place into the Default SE group '
+            'instead.\r\n\r\n'
+            '* The script has been primarily written for and tested with '
+            'Linux Server/VMWare/AWS clouds - it may not work as-is for other '
+            'clouds.\r\n\r\n'
+            '* Under bash on Linux, remember to escape the * character with a '
+            'backslash if passed as a parameter (e.g. in -v).\r\n')
